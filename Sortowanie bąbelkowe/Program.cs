3e10165a1d08b5d96b5d5a56aa4893f363859912
kacperkirszewski <144985740+kirszewskikacper@users.noreply.github.com>
2026-09-04 15:10:32 +0200

    Program z sortowaniem bąbelkowym

---
+++ Sortowanie bąbelkowe/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Sortowanie_bąbelkowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array = GetArray(ArrayLenght());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nTablica przed sortowaniem:\n");
+            Console.ResetColor();
+            DispalyArray(array);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nTablica po sortowaniu:\n");
+            Console.ResetColor();
+            BubbleSort(array);
+            DispalyArray(array);
+            
+            
+        }
+
+        static float[] BubbleSort(float[] array)
+        {
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for(int i = 1; i<array.Length; i++)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        (array[i], array[i - 1]) = (array[i - 1], array[i]);
+                        isSorted = false;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        static int ArrayLenght()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Podaj długoś tablicy");
+            Console.ResetColor();
+            int num;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if(num < 1)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nPodano błędne dane!");
+                Console.ResetColor();
+                Console.WriteLine("Podaj dowolną liczbę naturalną dodatnią\n");
+                num = ArrayLenght();
+            }
+            return num;
+        }
+
+        static void DispalyArray(float[] array)
+        {
+            for (int i = 0; i<array.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. element: {array[i]}");
+            }
+        }
+
+        static float[] GetArray(int lenght)
+        {
+            float[] array = new float[lenght];
+            for(int i = 0; i<lenght; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"\nPodaj element numer {i+1}");
+                Console.ResetColor();
+                if(float.TryParse(Console.ReadLine(), out float number))
+                {
+                    array[i] = number;      
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nPodano błędne dane!");
+                    Console.ResetColor();
+                    Console.WriteLine("Podaj dowolną liczbę zmiennoprzecinkową\n");
+                    i--;
+                }
+            }
+            return array;
+        }
+    }
+}
